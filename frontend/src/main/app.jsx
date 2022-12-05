eee8bf32271335ba24547c56a4af428a17bf8496
Bruno César <brunocesarmg@gmail.com>
2022-12-05 16:51:19 -0300

    Dependências do template (AdminLTE)

--- frontend/src/main/app.jsx
+++ frontend/src/main/app.jsx
@@ -1,8 +1,8 @@
+import "../common/template/dependencies";
 import React from "react";
 
 export default (props) => (
   <div className="wrapper">
     <h1>App</h1>
-    <p>My App is running!</p>
   </div>
 );
