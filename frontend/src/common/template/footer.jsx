c5b64cb9119310d658c17c29c62a068080f3d253
Bruno César <brunocesarmg@gmail.com>
2022-12-05 18:03:53 -0300

    Componente rodapé

---
+++ frontend/src/common/template/footer.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+export default (props) => (
+  <footer className="main-footer">
+    <strong>
+      CopyLeft &copy; 2022
+      <a href="https://www.linkedin.com/in/brunocesarti" target="_blank">
+        {" "}
+        Bruno César
+      </a>
+      .
+    </strong>
+  </footer>
+);
