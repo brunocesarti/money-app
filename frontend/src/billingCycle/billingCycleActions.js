4e13015f634ecc1695268d102bf9c3ac1a35a421
Bruno César <brunocesarmg@gmail.com>
2023-01-07 12:07:15 -0300

    Exibindo as mensagens de sucesso e erro

--- frontend/src/billingCycle/billingCycleActions.js
+++ frontend/src/billingCycle/billingCycleActions.js
@@ -1,4 +1,5 @@
 import axios from 'axios';
+import { toastr } from 'react-redux-toastr';
 
 const BASE_URL = 'http://localhost:3004/api';
 
@@ -11,7 +12,11 @@ export function getList() {
 }
 
 export function create(values) {
-    axios.post(`${BASE_URL}/billingCycles`, values);
+    axios.post(`${BASE_URL}/billingCycles`, values).then(resp => {
+        toastr.success('Sucesso', 'Operação realizada com sucesso.')
+    }).catch(e => {
+        e.response.data.errors.forEach(error => toastr.error('Erro', error));
+    });
     return {
         type: 'TEMP'
     } 
