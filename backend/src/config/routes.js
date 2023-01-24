81927f5cdd43e29f54c75f6fd28a263960c463ad
Bruno César <brunocesarmg@gmail.com>
2023-01-24 18:45:24 -0300

    Autenticação backend - parte 9

--- backend/src/config/routes.js
+++ backend/src/config/routes.js
@@ -7,6 +7,7 @@ module.exports = function (server) {
    */
   const protectedApi = express.Router();
   server.use("/api", protectedApi);
+
   protectedApi.use(auth);
   const BillingCycle = require("../api/billingCycle/billingCycleService");
   BillingCycle.register(protectedApi, "/billingCycles");
@@ -15,6 +16,7 @@ module.exports = function (server) {
    */
   const openApi = express.Router();
   server.use("/oapi", openApi);
+
   const AuthService = require("../api/user/AuthService");
   openApi.post("/login", AuthService.login);
   openApi.post("/signup", AuthService.signup);
